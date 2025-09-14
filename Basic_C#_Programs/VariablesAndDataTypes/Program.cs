0fd6def762925fb143dadda6c92a8a87e09edbdb
ThudRogers <josephmleiter@gmail.com>
2025-09-14 19:12:48 -0400

    Add files via upload

---
+++ Basic_C#_Programs/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //int favoriteNumber = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            //int total = favoriteNumber + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine(); 
+        }
+    }
+}
