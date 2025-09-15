e613e524924f02053e1c498eb50df6badac1000d
ThudRogers <josephmleiter@gmail.com>
2025-09-15 11:12:08 -0400

    Add files via upload

---
+++ MathOperatorProgram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal; 
+            //Console.WriteLine(combined);
+            //Console.ReadLine(); 
+
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Five minus Ten = " + difference.ToString());
+            //Console.ReadLine(); 
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine(); 
+
+            //double  quotient = 100.0 / 17.0; 
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 13 % 2;
+            //Console.WriteLine(remainder); 
+
+            //int num1 = 200 / 3;
+            //Console.WriteLine(num1);
+
+            //bool trueOrFalse = 12 > 5; 
+            //Console.WriteLine(trueOrFalse);
+            //Console.ReadLine();
+
+            //int roomTemperature = 70;
+            //int currentTemperature = 72;
+
+            //bool isWarm = currentTemperature >= roomTemperature; 
+            //Console.WriteLine(isWarm);
+            //Console.ReadLine();
+
+            
+
+
+
+        }
+    }
+}
