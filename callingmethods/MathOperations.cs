147547ccf849ec8bb55f9edce2e488b1694e9147
ThudRogers <josephmleiter@gmail.com>
2025-10-02 11:44:24 -0400

    Implement MathOperations class with basic methods

--- callingmethods/MathOperations.cs
+++ callingmethods/MathOperations.cs
@@ -1,6 +1,22 @@
-﻿internal class MathOperations
+using System;
+
+public class MathOperations
 {
-    public MathOperations()
+    // Method 1: Square the number
+    public int Square(int number)
     {
+        return number * number;
+    }
+
+    // Method 2: Double the number
+    public int Double(int number)
+    {
+        return number * 2;
+    }
+
+    // Method 3: Add 10 to the number
+    public int AddTen(int number)
+    {
+        return number + 10;
     }
 }
