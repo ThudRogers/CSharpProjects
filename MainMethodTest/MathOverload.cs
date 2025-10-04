5bcfc6780a4d6eeaf9d14c7b11505cc3e4212d15
ThudRogers <josephmleiter@gmail.com>
2025-10-04 11:34:23 -0400

    Add files via upload

---
+++ MainMethodTest/MathOverload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MathOverload
+{
+    // Method 1: Takes an integer, performs addition
+    public int DoMath(int number)
+    {
+        return number + 10;
+    }
+
+    // Method 2: Takes a decimal, performs subtraction (returns int)
+    public int DoMath(decimal number)
+    {
+        return (int)(number - 5);
+    }
+
+    // Method 3: Takes a string, converts to int if possible, performs multiplication
+    public int DoMath(string numberString)
+    {
+        int num = Convert.ToInt32(numberString);
+        return num * 3;
+    }
+}
---
+++ MainMethodTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create an instance of the MathOverload class
+        MathOverload mathOps = new MathOverload();
+
+        // Integer version
+        int result1 = mathOps.DoMath(10);
+        Console.WriteLine($"Integer method result: {result1}");
+
+        // Decimal version
+        int result2 = mathOps.DoMath(15.5m);
+        Console.WriteLine($"Decimal method result: {result2}");
+
+        // String version
+        int result3 = mathOps.DoMath("7");
+        Console.WriteLine($"String method result: {result3}");
+
+        Console.WriteLine("\nProgram finished. Press Enter to exit...");
+        Console.ReadLine();
+    }
+}
